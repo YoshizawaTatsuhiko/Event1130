b40d27d11bde6e2e2ea20a040847ef50d7332b5f
sato_310 <fuuuma.s@gmail.com>
2022-11-30 13:35:55 +0900

    スコアマネージャー変更

--- Assets/sato_fuma/scoremanager.cs
+++ Assets/sato_fuma/scoremanager.cs
@@ -2,42 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scoremanager : MonoBehaviour
+public class ScoreManager : MonoBehaviour
 {
-    public int score; // スコア
+    private int _score; // スコア
     //private int minusScorepoint = 10; //減少するスコアのポイント
     private int _highScore; //ハイスコア
     private float _time; //経過時間
-    private float maxtime = 100.0f; //制限時間
+    private float _maxTime = 100.0f; //制限時間
     private bool ingame = true; 
     // Start is called before the first frame update
     void Start()
     {
-        score = 0;
+        _score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(_highScore < score) //もしハイスコアを更新したら
+        if(_highScore < _score) //もしハイスコアを更新したら
         {
-            _highScore = score; //ハイスコアをスコアに変え、セーブする
+            _highScore = _score; //ハイスコアをスコアに変え、セーブする
             PlayerPrefs.SetInt("score", _highScore);
             PlayerPrefs.Save();
         }
         if(ingame == true)
         {
-            _time += Time.deltaTime ; //タイマー
-            if (maxtime < _time) //タイムアップ時
+            _time += Time.deltaTime; //タイマー
+            if (_maxTime < _time) //タイムアップ時
             {
                 print("gameorver");
                 ingame = false;
             }
         }
     }
-    public void Addscore(int plusScorepoint) //スコアを増加させるメゾット
+    public void AddScore(int plusScorepoint) //スコアを増加させるメゾット
     {
-        score += plusScorepoint;
+        _score += plusScorepoint;
     }
     //public void lowerscore() //スコアを減少させるメゾット
     //{

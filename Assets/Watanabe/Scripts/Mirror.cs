79131822306cae8ca7297c115be7c49b71373808
Akira Watanabe <a0323w@gmail.com>
2022-11-30 15:50:49 +0900

    [update] オブジェクトの反射

--- Assets/Watanabe/Scripts/Mirror.cs
+++ Assets/Watanabe/Scripts/Mirror.cs
@@ -15,12 +15,13 @@ public class Mirror : MonoBehaviour
         //当たったオブジェクトを跳ね返す
         Debug.Log("跳ね返します");
         var hitRb = col.gameObject.GetComponent<Rigidbody2D>();
-        var reflect = Vector2.Reflect(hitRb.velocity, transform.up);
+        //var reflect = Vector2.Reflect(hitRb.velocity, transform.up);
         //Vector2.Reflect(a, b)
         //a...入射ベクトル
         //b...当たった面の法線ベクトル(正規化されている必要がある)
 
-        hitRb.velocity = reflect * _reflectPower;
+        hitRb.velocity *= _reflectPower * -1;
+        //hitRb.velocity = reflect * _reflectPower;
         col.gameObject.layer = _layerNum;
     }
 }

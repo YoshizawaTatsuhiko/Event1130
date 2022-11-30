93998718f19279f18c02a2fefad97f1e04a1e59f
Akira Watanabe <a0323w@gmail.com>
2022-11-30 16:54:12 +0900

    [update] 鏡を出している間は動かない, 鏡のLayer判定

--- Assets/Watanabe/Scripts/Mirror.cs
+++ Assets/Watanabe/Scripts/Mirror.cs
@@ -6,22 +6,27 @@ public class Mirror : MonoBehaviour
 {
     [Tooltip("反射する力")]
     [SerializeField] private float _reflectPower = 1f;
-    [Tooltip("Layerの番号")]
-    [SerializeField] private int _layerNum = 0;
+    [Tooltip("反射した隕石のLayerの番号")]
+    [SerializeField] private int _reflectLayerNum = 0;
+    [Tooltip("当たったオブジェクトのLayerの番号")]
+    [SerializeField] private int _hitLayerNum = 0;
 
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        //当たったオブジェクトを跳ね返す
-        Debug.Log("跳ね返します");
-        var hitRb = col.gameObject.GetComponent<Rigidbody2D>();
-        //var reflect = Vector2.Reflect(hitRb.velocity, transform.up);
-        //Vector2.Reflect(a, b)
-        //a...入射ベクトル
-        //b...当たった面の法線ベクトル(正規化されている必要がある)
+        if (col.gameObject.layer == _hitLayerNum)
+        {
+            //当たったオブジェクトを跳ね返す
+            Debug.Log("跳ね返します");
+            var hitRb = col.gameObject.GetComponent<Rigidbody2D>();
+            //var reflect = Vector2.Reflect(hitRb.velocity, transform.up);
+            //Vector2.Reflect(a, b)
+            //a...入射ベクトル
+            //b...当たった面の法線ベクトル(正規化されている必要がある)
 
-        hitRb.velocity *= _reflectPower * -1;
-        //hitRb.velocity = reflect * _reflectPower;
-        col.gameObject.layer = _layerNum;
+            hitRb.velocity *= _reflectPower * -1;
+            //hitRb.velocity = reflect * _reflectPower;
+            col.gameObject.layer = _reflectLayerNum;
+        }
     }
 }
